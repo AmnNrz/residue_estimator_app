44295b2ac60bfbceb0934e37493012d4d65ed354
Amin Norouzi <amnnrz@PACCAR.PC>
2024-07-10 13:12:34 -0700

    read images and prepare dataset for training

--- segmentation_pipeline.py
+++ segmentation_pipeline.py
@@ -37,7 +37,7 @@ from sklearn.pipeline import Pipeline
 import pickle
 import logging
 import glob
-
+from collections import defaultdict
 from utils_segmentation import (
     get_features,
     p3,
@@ -52,7 +52,7 @@ from utils_segmentation import (
 logging.basicConfig(level=logging.INFO)
 
 # +
-##### Read an rgb image with its labels
+##### Read an rgb image with its labels #####
 path_to_data = ("/mnt/C250892050891BF3/Projects/residue_estimator/images/")
 
 ### Function to find images
@@ -61,7 +61,6 @@ def find_jpg_images(root_dir):
     for dir_name,a,b in os.walk(root_dir):
         search_pattern = os.path.join(dir_name, '*.jpg')
         for filename in glob.glob(search_pattern):
-            print(filename)
             jpg_image.append(filename)
     return jpg_image    
 
@@ -74,17 +73,71 @@ def find_tif_labels(root_dir):
             tif_labels.append(filename)
     return tif_labels
 
-### Read original image
+# Check if labels exist for each image
 path_to_original = path_to_data + "original/"
-original_files = os.listdir(path_to_original)
+original_paths = find_jpg_images(path_to_original)
 
-### Read labels
 path_to_labels = path_to_data + "label/"
-find_tif_labels(path_to_labels)
+label_paths = find_tif_labels(path_to_labels)
 
+# Extract image numbers from file paths
+def extract_image_number(file_path):
+    base_name = os.path.basename(file_path)
+    return base_name.split('_')[1].split('.')[0]
 
+# Group labels by image number
+label_dict = defaultdict(set)
+for label in label_paths:
+    image_number = extract_image_number(label)
+    label_dict[image_number].add(label)
 
+# Find valid image numbers that have both _res.tif and _sunshad.tif
+valid_image_numbers = {
+    image_number for image_number, files in label_dict.items()
+    if len(files) == 2 and any('_res.tif' in file
+                                for file in files) and 
+                                any('_sunshad.tif' in file for file in files)
+}
 
+# Filter original images and labels
+filtered_original_images = [img for img in original_paths if
+                             extract_image_number(img) in valid_image_numbers]
+filtered_labels = [label for label in label_paths if
+                    extract_image_number(label) in valid_image_numbers]
 
+# List excluded images
+excluded_images = [img for img in original_paths if 
+                   extract_image_number(img) not in valid_image_numbers]
 
+# # Print results
+# print("Filtered Original Images:")
+# print(filtered_original_images)
+# print("\nFiltered Labels:")
+# print(filtered_labels)
+# print("\nExcluded Images:")
+# print(excluded_images)
+dataset = []
+for path in filtered_original_images:
+    print()
+    bgr = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+    image_number = os.path.basename(path).split('.')[0]
+    label_paths = [path for path in filtered_labels
+                    if path.endswith(f'{image_number}' + '_res.tif')
+                    or path.endswith(f'{image_number}' + '_sunshad.tif')]
+    res_label = cv2.imread(label_paths[0], cv2.IMREAD_UNCHANGED)
+    sunshad_label = cv2.imread(label_paths[1], cv2.IMREAD_UNCHANGED)
+    comb_label = 2 * res_label + sunshad_label
+    features = get_features(bgr)
 
+    res_label = res_label.ravel()
+    sunshad_label = sunshad_label.ravel()
+    comb_label = comb_label.ravel()
+
+    dataset.append({"bgr": bgr, 
+                    "features": features,
+                    "res_label": res_label,
+                    "sunshad_label": sunshad_label})
+    
+dataset
+
+    # res_label = cv2.imread()
